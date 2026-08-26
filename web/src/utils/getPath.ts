31e903e6c5696ea2824aaf1fb3bb8dbe435d7cb5
jighog1 <jighog1@gmail.com>
2026-08-26 16:26:41 +0900

    content: remove low-quality posts before 2026-08-15

--- web/src/utils/getPath.ts
+++ web/src/utils/getPath.ts
@@ -3,7 +3,7 @@ export function getPath(
   _filePath: string | undefined,
   includeBase = true
 ) {
-  // id is like "auto-post-20260407162620" or "subdir/post-name"
+  // id is like "auto-post-YYYYMMDDHHMMSS" or "subdir/post-name"
   // We only want the last segment (the slug itself), ignoring sub-directories
   const slug = id.split("/").pop() ?? id;
 
